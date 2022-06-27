7f47cc6d11e0c2bc3e37c90e439d7508b0f3a40f
amoeba224 <amoeba224@likelion.org>
2022-06-27 20:53:10 +0900

    need to redirect

--- components/common/Write.js
+++ components/common/Write.js
@@ -7,7 +7,9 @@ import axios from 'axios';
 
 export default function Write(props){
     const [categories, setCategories] = useState([]);
-    const [usingCategory, setUsingCategory] = useState({"title":"선택하세요"});
+    const [usingCategory, setUsingCategory] = useState("");
+    const [title, setTitle] = useState("");
+    const [body, setBody] = useState("");
 
     const getCategories = async () => {
         const {data} = await axios.get("/api/category");
@@ -18,9 +20,16 @@ export default function Write(props){
     useEffect(()=>{
         getCategories();
         if (props.props) {
-            console.log(props.props)
+            axios.get(`/api/notice/${props.props}`)
+            .then((res)=>{
+                const data = res.data
+                setUsingCategory(data.category);
+                setTitle(data.title);
+                setBody(data.body);
+            })
+            .catch((err)=>console.log(err))
         }
-    });
+    }, []);
 
     return (
     <article>
@@ -30,7 +39,7 @@ export default function Write(props){
             event.preventDefault();
             const title = event.target.title.value;
             const body = event.target.body.value;
-            const category = usingCategory.title;
+            const category = usingCategory;
             const writer = '문태주';
             const notice = {
                 title:title,
@@ -45,15 +54,15 @@ export default function Write(props){
         }}>
             <Dropdown>
         <Dropdown.Toggle variant="success" id="dropdown-basic" size="sm">
-            {usingCategory.title}
+            {usingCategory}
         </Dropdown.Toggle>
 
         <Dropdown.Menu>
-            {categories.map((elem)=><Dropdown.Item onClick={()=>setUsingCategory(elem)}key={elem._id}>{elem.title}</Dropdown.Item>)}
+            {categories.map((elem)=><Dropdown.Item onClick={()=>setUsingCategory(elem.title)}key={elem._id}>{elem.title}</Dropdown.Item>)}
         </Dropdown.Menu>
         </Dropdown>
-            <p><input type="text" name="title" placeholder="제목"/></p>
-            <p><textarea name="body" placeholder="내용을 입력하세요"></textarea></p>
+            <p><input type="text" name="title" placeholder="제목" value={title} onChange={(e)=>setTitle(e.target.value)}/></p>
+            <p><textarea name="body" placeholder="내용을 입력하세요" value={body} onChange={(e)=>setBody(e.target.value)}></textarea></p>
             <p><input type="submit" value="저장"></input></p>
         </form>
     </article>
