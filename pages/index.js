70db9d426ded98d33577501e2d7c4ce80da0cf35
Daniel2231 <daniel2231.dev@gmail.com>
2022-06-30 20:00:59 +0900

    footer fix

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,19 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import 'bootstrap/dist/css/bootstrap.min.css';
-import CustomNavbar from '../components/common/Navbar'
-import styled from '@emotion/styled';
-import Image from 'next/image';
-import Likelion from '../public/likelionuniv.png';
-import SKKU from '../public/skku.jpg';
-import Bgimg from '../public/bgimg1.jpg';
-import StyledFooter from '../components/common/Footer';
-import Carousel from 'react-bootstrap/Carousel';
-import Link from 'next/link';
+import "bootstrap/dist/css/bootstrap.min.css";
+import CustomNavbar from "../components/common/Navbar";
+import styled from "@emotion/styled";
+import Image from "next/image";
+import Likelion from "../public/likelionuniv.png";
+import SKKU from "../public/skku.jpg";
+import Bgimg from "../public/bgimg1.jpg";
+import StyledFooter from "../components/common/Footer";
+import Carousel from "react-bootstrap/Carousel";
+import Link from "next/link";
 
 function UncontrolledExample() {
   return (
     <>
-      <CustomNavbar name="Likelion SKKU Notice"/>
+      <CustomNavbar name="Likelion SKKU Notice" />
       <Box>
         <h1>멋사 공지 한눈에!</h1>
         <h4>여러가지 공지를 모두 확인하세요.</h4>
@@ -22,7 +22,7 @@ function UncontrolledExample() {
           <Carousel>
             <Carousel.Item>
               <Link href="/center">
-                <StyledImage src={Likelion} alt="center"/>
+                <StyledImage src={Likelion} alt="center" />
               </Link>
               <Carousel.Caption>
                 <Styledh3>중앙 공지</Styledh3>
@@ -30,7 +30,7 @@ function UncontrolledExample() {
             </Carousel.Item>
             <Carousel.Item>
               <Link href="/skku">
-                <StyledImage src={SKKU} alt="skku"/>
+                <StyledImage src={SKKU} alt="skku" />
               </Link>
               <Carousel.Caption>
                 <Styledh3>성대 공지</Styledh3>
@@ -38,7 +38,7 @@ function UncontrolledExample() {
             </Carousel.Item>
             <Carousel.Item>
               <Link href="/study">
-                <StyledImage src={Bgimg} alt="study"/>
+                <StyledImage src={Bgimg} alt="study" />
               </Link>
               <Carousel.Caption>
                 <Styledh3>스터디 공지</Styledh3>
@@ -53,26 +53,29 @@ function UncontrolledExample() {
 }
 
 const Box = styled.div`
-display: flex;
-flex-direction: column;
-justify-content: center;
-align-items: center;
-margin-top: 70px;`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-top: 70px;
+`;
 
 const SizedCarousel = styled.div`
   width: 300px;
   height: 380px;
-  `
+`;
 const StyledImage = styled(Image)`
-  border-radius : 25px;
-  `
+  border-radius: 25px;
+  cursor: pointer;
+`;
+
 const Styledh3 = styled.h5`
-  background-color: #FF9E1B;
+  background-color: #ff9e1b;
   width: auto;
   border-radius: 20px;
   color: black;
   width: 120px;
   margin-left: 45px;
-  `
+`;
 
 export default UncontrolledExample;
